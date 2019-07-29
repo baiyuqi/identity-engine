c596641cea8f58c724a354f84ae9c2d2fb8f2639
baiboshi <yuqi.bai@albus.com.cn>
2019-07-29 14:22:15 +0800

    hh

--- resolver.ts
+++ resolver.ts
@@ -4,6 +4,6 @@ import registerResolver from 'ethr-did-resolver'
 registerResolver()
 
 // You can also use ES7 async/await syntax
-resolve('did:ethr:0xf3beac30c498d9e26865f34fcaa57dbb935b0d74').then(doc=>{
+resolve('did:ethr:0xf3beac30c498d9e26865f34fcaa57dbb935b0d79').then(doc=>{
     console.log(doc);
 })
